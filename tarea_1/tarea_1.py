8193674ca7b30ac3d150f26428451f3b4b60799b
Adecuado <gabrielgarces2013@gmail.com>
2026-03-23 22:22:11 -0300

    empeze a hacer el hexa a octal pero todavia no funciona por que me quiero acostar

--- tarea_1/tarea_1.py
+++ tarea_1/tarea_1.py
@@ -11,14 +11,17 @@ n = 1
 
 # Al final si o si deberiamos (probablemente) hacer la transformacion intermedia a decimal ya que las herramientas/operadores que tenemos son para trabajar en esta base
 
+
 def convertor_general(num_original, base_origen, base_destino):
-    decimal_a_hexadecimal = {10 : "A", 11 : "B", 12 : "C", 13 : "D", 14 : "E", 15 : "F"}
-    hexadecimal_a_decimal = {valor : clave for clave,valor in decimal_a_hexadecimal.items()}
+    decimal_a_hexadecimal = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
+    hexadecimal_a_decimal = {
+        valor: clave for clave, valor in decimal_a_hexadecimal.items()
+    }
     total = 0
     potencia_actual = 0
     # Se convierte el numero a base 10, multiplicando cada digito por la pontencia que le corresponderia en base 10. ademas de considerar el caso
     # de que hubieran caracteres hexadecimales como A,B,C...
-    for digit in str(num_original)[::-1]: # asi voltean los string la gente sin atun
+    for digit in str(num_original)[::-1]:  # asi voltean los string la gente sin atun
         if digit in hexadecimal_a_decimal:
             digit = hexadecimal_a_decimal[digit]
         total += int(digit) * base_origen**potencia_actual
@@ -47,11 +50,11 @@ def convertor_general(num_original, base_origen, base_destino):
             resultado += str(next_num)
     return resultado
 
+
 # Aprovecharé conversor_general para forzar la base decimal y filtrar según rango ASCII (32 - 126)
-def filtro_ascii_valido(numero,base_origen):
-    numero_en_decimal = int(convertor_general(numero,base_origen,10))
+def filtro_ascii_valido(numero, base_origen):
+    numero_en_decimal = int(convertor_general(numero, base_origen, 10))
     if numero_en_decimal >= 32 and numero_en_decimal <= 126:
-        
         # En este punto numero_en_decimal será cada numero en orden que sea válido dentro del rango ascci, por lo que se puede decodificar el mensaje e irlo almacenando
         # Conviene hacer la traducción ASCII debido a que los caracteres son válidos, se usa una variable global para ir almacenando el mensaje
         global mensaje
@@ -59,12 +62,11 @@ def filtro_ascii_valido(numero,base_origen):
 
         ## ESTAS 2 LINEAS SON PARA VER SI LA CONVERSIÓN SE HACIA CORRECTAMENTE, HAY QUE BORRARLAS DESPUÉS, PERO SE VE QUE EL 0x68 se traduce a 104 en base 10 pero la tarea
         # lo consdiera como di fuera 100, pq 100 es "d" en ASCII, pero al hacer la conversión correcta da 104, que es "h" en ASCII
-        #print("el numero es" , numero, "en", base_origen)
-        #print(numero_en_decimal)
+        # print("el numero es" , numero, "en", base_origen)
+        # print(numero_en_decimal)
 
         ##
         return True
-
 
     # Else pa demostrar que lo hice yo y no la ia :v (completamente forzado) [ademas la ia no usa variables globales 😎]
     else:
@@ -87,13 +89,14 @@ def leer_numero_completo(digitos_validos, file, base_origen, base_destino_elegid
     file.seek(file.tell() - 1)
 
     # Esto evita lectura de caracter vació, asique luego de esta comprobación puedo hacer el filtrado ASCII y deberia funcionar por cortocircuito
-    if numero_completo and filtro_ascii_valido(numero_completo,base_origen):
+    if numero_completo and filtro_ascii_valido(numero_completo, base_origen):
         global n
         print(
             f"valor {n}: {convertor_general(numero_completo, base_origen, base_destino_elegida)} "
             f"(Original: {NBO}{numero_completo})"
         )
-        n+=1
+        n += 1
+
 
 print("--- DECODIFICADOR DE NOTAS ---\n")
 base_destino_elegida = 0
@@ -111,13 +114,21 @@ while True:
     curr_char = file.read(1)
     # print(curr_char)
     if curr_char == "*":
-        leer_numero_completo(DIGITOS_VALIDOS_BINARIO, file, 2, base_destino_elegida,"Binario *")
+        leer_numero_completo(
+            DIGITOS_VALIDOS_BINARIO, file, 2, base_destino_elegida, "Binario *"
+        )
     elif curr_char == "&":
-        leer_numero_completo(DIGITOS_VALIDOS_OCTAL, file, 8, base_destino_elegida,"Octal &")
+        leer_numero_completo(
+            DIGITOS_VALIDOS_OCTAL, file, 8, base_destino_elegida, "Octal &"
+        )
     elif curr_char == "#":
-        leer_numero_completo(DIGITOS_VALIDOS_DECIMAL, file, 10, base_destino_elegida,"Decimal #")
+        leer_numero_completo(
+            DIGITOS_VALIDOS_DECIMAL, file, 10, base_destino_elegida, "Decimal #"
+        )
     elif curr_char == "!":
-        leer_numero_completo(DIGITOS_VALIDOS_HEXADECIMAL, file, 16, base_destino_elegida, "Hexadecimal !")
+        leer_numero_completo(
+            DIGITOS_VALIDOS_HEXADECIMAL, file, 16, base_destino_elegida, "Hexadecimal !"
+        )
     # Si file.read() lee un "" significa que el archivo ya termino
     elif curr_char == "":
         break
@@ -125,5 +136,6 @@ while True:
 
 print("\nMENSAJE DECODIFICADO:\n", mensaje)
 
-# Una vez a un loco le pusieron un 0 en el certamen de progra pq no cerró el archivo :v 
+# Una vez a un loco le pusieron un 0 en el certamen de progra pq no cerró el archivo :v
 file.close()
+
